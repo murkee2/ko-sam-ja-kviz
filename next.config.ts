ae044e4ae229a85ab37ce11634bda76db6622540
murkee2 <162224316+murkee2@users.noreply.github.com>
2026-08-23 02:51:27 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Opciono: Ako ti css ili slike ne budu radile, otkomentariši liniju ispod
+  // basePath: '/ko-sam-ja-kviz', 
+};
 
 export default nextConfig;
